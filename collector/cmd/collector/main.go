af360ca51d87a2e35f6665cb528ce2ad780a694a
Ivan Babrou <ibobrik@gmail.com>
2015-08-16 16:02:21 +0100

    Support for TLS connections to docker

--- collector/cmd/collector/main.go
+++ collector/cmd/collector/main.go
@@ -7,27 +7,37 @@ import (
 
 	"github.com/bobrik/collectd-docker/collector"
 	"github.com/fsouza/go-dockerclient"
+	"path"
 )
 
 func main() {
-	endpoint := flag.String("endpoint", "unix:///var/run/docker.sock", "docker endpoint")
-	host := flag.String("host", "", "host to report")
-	interval := flag.Int("interval", 1, "interval to report")
+	e := flag.String("endpoint", "unix:///var/run/docker.sock", "docker endpoint")
+	c := flag.String("cert", "", "cert path for tls")
+	h := flag.String("host", "", "host to report")
+	i := flag.Int("interval", 1, "interval to report")
 	flag.Parse()
 
-	if *host == "" {
+	if *h == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	client, err := docker.NewClient(*endpoint)
+	var client *docker.Client
+	var err error
+
+	if *c != "" {
+		client, err = docker.NewTLSClient(*e, path.Join(*c, "cert.pem"), path.Join(*c, "key.pem"), path.Join(*c, "ca.pem"))
+	} else {
+		client, err = docker.NewClient(*e)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	writer := collector.NewCollectdWriter(*host, os.Stdout)
+	writer := collector.NewCollectdWriter(*h, os.Stdout)
 
-	collector := collector.NewCollector(client, writer, *interval)
+	collector := collector.NewCollector(client, writer, *i)
 
 	err = collector.Run(5)
 	if err != nil {
